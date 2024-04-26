33c3f5c42ea94e3ca9939368cec819a1c6e88f8c
andreltlc <andrecarvalho1402@gmail.com>
2024-04-26 10:31:38 -0300

    Alterando o que estava deixando login errado

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 import React from 'react';  
 import FirstAccess from './utils/pages/FirstAccess/FirstAsccess.js';
-import Login from './utils/pages/login';
+import Login from './utils/pages/Login/login';
 
 function App(props) {
   return (
-    <Login />
+    <Login/>
   );
 }
 
